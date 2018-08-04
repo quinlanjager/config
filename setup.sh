8001a042bd5fee041f2aa2daf7b95485ef486069
Quinlan Jager <quinlan.jager@gmail.com>
2018-08-04 08:37:22 -0700

    Update setup commands

--- setup.sh
+++ setup.sh
@@ -1,16 +1,9 @@
 brew install fish
 brew install git
 
-# Add vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim -c "PlugInstall"
-
-fish
-
 # link config files
-ln -s ./vimrc ~/.vimrc
-ln -s ./config.fish ~/.config/fish/config.fish
+ln ./vimrc ~/.vimrc
+ln ./config.fish ~/.config/fish/config.fish
 
 # Install omf and get the theme!
 curl -L https://get.oh-my.fish | fish
@@ -26,3 +19,10 @@ mkdir -p ~/.config/fish/completions; and cp ~/.asdf/completions/asdf.fish ~/.con
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
 asdf install nodjs
+
+# Add vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim -c "PlugInstall"
+
+
